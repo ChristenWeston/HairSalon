1e7ecda55f2b7fa818e64772538f0534e7c541a8
Christen Weston <paleeserecycle@gmail.com>
2022-03-12 15:39:52 -0800

    Added Page Title to Home page

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace HairSalon.Controllers
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewBag.PageTitle = "Sandra's Salon";
         return View();
       }
     }
